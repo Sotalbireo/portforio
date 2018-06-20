a23d5ce02cb785ede48722a7a37b527308ed0074
Unknown <athene609.cyl@gmail.com>
2018-06-21 03:39:14 +0900

    update 20180621

--- src/assets/script/_Wave.ts
+++ src/assets/script/_Wave.ts
@@ -20,7 +20,7 @@ export default class Wave {
 		this.canvas = document.createElement('canvas');
 		this.ctx = this.canvas.getContext('2d')!;
 
-		document.querySelector('body')!.appendChild(this.canvas);
+		document.querySelector('body')!.insertAdjacentElement('afterbegin', this.canvas);
 		this.canvas.className = 'js-no-print';
 		this.canvas.height = this.height;
 		this.canvas.width = this.width;
